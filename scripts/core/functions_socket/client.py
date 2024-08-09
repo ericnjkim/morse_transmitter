7844cc118f3839c6bc9280c798cb557689900d9b
ericnjkim <ericnjkim@gmail.com>
2024-08-09 18:34:43 +1000

    trying to get the new server client scripts to work. Not yet working.

--- scripts/core/functions_socket/client.py
+++ scripts/core/functions_socket/client.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import os
 
 
 class Client(threading.Thread):
@@ -8,42 +9,47 @@ class Client(threading.Thread):
         super().__init__()
         self.server_host = server_host
         self.server_port = server_port
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.receive_thread = threading.Thread(target=self.receive_messages)
+        self.send_thread = threading.Thread(target=self.send_messages)
 
     def run(self):
-        self.client.connect((self.server_host, self.server_port))
+        print("test")
+        self.connection_socket.connect((self.server_host, self.server_port))
+        print("Message sender active")
+        # print("Message receiver active")
+        self.receive_thread.start()
+        self.send_thread.start()
 
-        receive_thread = threading.Thread(target=self.receive_messages)
-        receive_thread.start()
-
-        send_thread = threading.Thread(target=self.send_messages)
-        send_thread.start()
-
+    #
     def receive_messages(self):
         while True:
             try:
-                message = self.client.recv(1024).decode("utf-8")
+                message = self.connection_socket.recv(1024).decode("utf-8")
                 print(message)
             except:
-                print("Error")
-                self.client.close()
+                self.close_socket()
                 break
 
     def send_messages(self):
         while True:
             message = input("")
             if message == "/disconnect":
-                self._disconnect_client()
+                self.close_socket()
                 break
-            self.client.send(message.encode("utf-8"))
-
-    def _disconnect_client(self):
-        self.client.send("/disconnect".encode("utf-8"))
+            self.connection_socket.send(message.encode("utf-8"))
+    #
+    def close_socket(self):
+        try:
+            self.connection_socket.send("Recipient disconnected".encode("utf-8"))
+        except:
+            pass
         print("Disconnecting...")
-        self.client.close()
-
+        self.connection_socket.close()
+        os._exit(0)
 
 host = socket.gethostbyname(socket.gethostname())
 port = 5050
 client = Client(host, port)
-client.start_client()
+client.start()
